dc984ed489e6764075d405faf63a46166fbba92b
mthakur <manish.thakur@agoda.com>
2022-03-11 16:20:08 +0700

    car shape test with draw and translate method

---
+++ src/test/java/com/patterns/template/CarShapeTest.java
@@ -0,0 +1,15 @@
+package com.patterns.template;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarShapeTest {
+    @Test
+    @DisplayName("draws car shape by using primitive methods translate & draw")
+    public void drawCarShape() {
+        SelectableShape shape = new CarShape();
+        Assertions.assertEquals("drawing car shape", shape.draw());
+        Assertions.assertEquals("translating to new point", shape.translate());
+    }
+}
