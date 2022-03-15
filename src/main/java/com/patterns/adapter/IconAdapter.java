ed5ff2b81f22d61f003e3c5afb6cd511ae444400
mthakur <manish.thakur@agoda.com>
2022-03-15 15:42:31 +0700

    Adapter pattern to convert an icon to a component

---
+++ src/main/java/com/patterns/adapter/IconAdapter.java
@@ -0,0 +1,23 @@
+package com.patterns.adapter;
+
+import javax.swing.*;
+import java.awt.*;
+
+class IconAdapter extends JComponent {
+
+    private final Icon icon;
+
+    public IconAdapter(Icon icon) {
+        this.icon = icon;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        icon.paintIcon(this, g, 0, 0);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(icon.getIconWidth(), icon.getIconHeight());
+    }
+}
