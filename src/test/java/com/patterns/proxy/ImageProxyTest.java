3e76a0e67938046f5792895bf2336a1dfcb945bf
mthakur <manish.thakur@agoda.com>
2022-04-25 14:43:25 +0700

    refatror code

--- src/test/java/com/patterns/proxy/ImageProxyTest.java
+++ src/test/java/com/patterns/proxy/ImageProxyTest.java
@@ -1,45 +1,47 @@
 package com.patterns.proxy;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.awt.*;
 
 public class ImageProxyTest {
+    ImageProxy proxy;
+    ImageProxy proxyMock;
+
+    @BeforeEach
+    public void setUp() {
+        proxy = new ImageProxy("what's in a name");
+        proxyMock = Mockito.spy(proxy);
+    }
+
     @Test
     public void assignsName() {
-        var proxy = new ImageProxy("what's in a name");
         Assertions.assertEquals("what's in a name", proxy.getName());
     }
 
     @Test
     public void paintingIconEnsuresImageLoading() {
-        var proxy = new ImageProxy("name");
-        ImageProxy proxyMock = Mockito.spy(proxy);
         proxyMock.paintIcon(Mockito.mock(Component.class), Mockito.mock(Graphics.class), 0, 1);
         Mockito.verify(proxyMock).ensureImageLoaded();
     }
 
     @Test
     public void accessingHeightIconEnsuresImageLoading() {
-        var proxy = new ImageProxy("name");
-        ImageProxy proxyMock = Mockito.spy(proxy);
         proxyMock.getIconHeight();
         Mockito.verify(proxyMock).ensureImageLoaded();
     }
 
     @Test
     public void accessingWidthIconEnsuresImageLoading() {
-        var proxy = new ImageProxy("name");
-        ImageProxy proxyMock = Mockito.spy(proxy);
         proxyMock.getIconWidth();
         Mockito.verify(proxyMock).ensureImageLoaded();
     }
 
     @Test
     public void imageLoadedBeforeUse() {
-        var proxy = new ImageProxy("image");
         proxy.ensureImageLoaded();
         Assertions.assertNotNull(proxy.getImage());
     }
