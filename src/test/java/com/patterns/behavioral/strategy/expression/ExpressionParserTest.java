d342c76d5884e665cb4912c40d595172b4ffd71e
mthakur <manish.thakur@agoda.com>
2022-07-20 12:56:29 +0700

    extract expression code

--- src/test/java/com/patterns/behavioral/strategy/expression/ExpressionParserTest.java
+++ src/test/java/com/patterns/behavioral/strategy/expression/ExpressionParserTest.java
@@ -1,21 +1,29 @@
 package com.patterns.behavioral.strategy.expression;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.util.List;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ExpressionParserTest {
+    private ExpressionParser p;
+
+    @BeforeAll
+    public void setUp() {
+        p = new ExpressionParser();
+    }
+
     @Test
     public void parseToAnOperandLiteral() {
-        ExpressionParser p = new ExpressionParser();
         List<Literal> list = p.parse("1");
         Assertions.assertEquals(1, ((Operand) list.get(0)).getVal());
     }
 
     @Test
     public void parseToAnOperatorLiteral() {
-        ExpressionParser p = new ExpressionParser();
         List<Literal> list = p.parse("+");
         Assertions.assertEquals("Add", list.get(0).getClass().getSimpleName());
     }
